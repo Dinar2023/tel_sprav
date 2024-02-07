69019c84d4d0d5afd3b0989f7e32a2e2c9f0de7f
Dinar <dinar-bogdanov@yandex.ru>
2024-02-07 21:27:26 +0500

    homework

--- tel_sprav.py
+++ tel_sprav.py
@@ -8,7 +8,7 @@ def read_sprav():
     # print(type(content))
     for line in tel_sprav:
         n = line.split()
-        # print (n)
+        print (n)
         dict_ = {
             "last_name": n[0],
             "first_name": n[1],
@@ -60,14 +60,47 @@ def add_contact(tel_spra_ls):
     # ToDo: использовать str.capitalize()
 
     tel_sprav.close()
-    return tel_sprav
+    return tel_spra_ls
 
 
 def find_last_name(last_name: str, my_sprav):
     for item in my_sprav:
-        if item["last_name"] == last_name.capitalize():
-            print(item)
+        if item["last_name"] == last_name.capitalize() or item["first_name"] == last_name.capitalize() or item["second_name"] == last_name.capitalize():
+            print(*(f"{v}" for v in item.values()))
 
+
+def change_human (tel_spra_ls):
+    print("Введите номер человека, которого хотите заменить")
+    for index, item in enumerate(tel_spra_ls):
+         print(*(f"{index+1} {v}" for v in item.values()))
+    number = int(input())-1
+    print(number)
+    dict_ = {                                            # Попробуй разобраться почему не работает
+        "last_name": input('Введите фамилию\t').capitalize(),        
+        "first_name": input('Введите имя\t').capitalize(),
+        "second_name":input('Введите отчество\t').capitalize(),
+        "tel": input('Введите телефон\t').capitalize(),
+        }
+
+    tel_spra_ls[number] = dict_ # Заменяем значение в массиве
+    print(dict_)
+    print(tel_spra_ls)
+    tel_sprav = open('tel_sprav.txt', 'w', encoding='utf-8')
+    for item in tel_spra_ls:
+        print (item)
+        for val in item.values():
+            # print (key, val)
+            tel_sprav.writelines(f"{val}\t")
+        tel_sprav.write(f" \n")
+ 
+    tel_sprav.close()
+
+
+
+
+
+
+    return (tel_spra_ls)
 
 def main():
     # переменная = open ('название файла', 'режим работы', encoding='кодировка')
@@ -76,6 +109,7 @@ def main():
         print("1: Вывести данные")
         print("2: Записать новый контакт")
         print("3: Найти контакт по фамилии")
+        print("4: Заменить контакт")
         print("0: Выйти")
 
         x = input()
@@ -88,6 +122,9 @@ def main():
             find_last_name(input("Введите фамилию: "), my_sprav=tel_sprav)
         elif x == "0":
             break
+        elif x == "4":
+            change_human(tel_sprav)
+                    
         else:
             print("неверная команда")
 
